3ba8ccbe3001bcd37e0d15f801de3e59311dcc62
etash-singh <etash.singh@hpe.com>
2020-06-25 12:01:10 +0530

    updated script

--- service_monitor_alert.py
+++ service_monitor_alert.py
@@ -8,18 +8,17 @@ from email.mime.text import MIMEText
 
 service = sys.argv[1]
 hostname = socket.gethostname()
-ip_address = "13.68.147.249"
 
 def send_email():
  msg = MIMEText("Service %s is down!! \nCheck the service %s immediately on host %s!"%(service,service,hostname))
  msg["Subject"] = "SERVICE %s DOWN"%(service.upper())
- msg["From"] = "etashsingh29@gmail.com"
- msg["To"] = "etash.singh@knoldus.com"
- with smtplib.SMTP("smtp.gmail.com", 587) as server:
+ msg["From"] = "sender@mail.com"
+ msg["To"] = "reciever@mail.com"
+ with smtplib.SMTP("example.smtp.com", 587) as server:
   server.ehlo()
   server.starttls()
-  server.login("etashsingh29@gmail.com","wutifrrziksqsvbp")
-  server.sendmail("etashsingh29@gmail.com","etash.singh@knoldus.com",msg.as_string())
+  server.login("sender@mail.com","PASSWORD")
+  server.sendmail("sender@mail.com","reciever@mail.com",msg.as_string())
 
 def check_service():
  p =  subprocess.Popen(["systemctl", "is-active",  service], stdout=subprocess.PIPE)
